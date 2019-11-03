6a8fcabeb4aa4cc66959baffa73a6351413583a3
Elucidateqt <elvirbajrami@hotmail.de>
2019-11-03 19:25:48 +0100

    restored health-mechanic, smoother difficulty, removed duplicate modals

---
+++ src/components/Healthbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <font-awesome-icon size="lg" class="mr-1" style="color:darkred" v-for="_ in current" icon="heart"></font-awesome-icon>
+    <font-awesome-icon size="lg" class="mr-1" style="color:darkgrey" v-for="_ in max-current" icon="heart"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Healthbar",
+  props: {
+    max: Number,
+    current: Number,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
