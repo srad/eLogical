bea20d96839f73d1920fe1237599675cbf8ab878
srad <ssedighi@posteo.de>
2019-11-05 23:49:01 +0100

    More precise linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  "extends": [
+    "plugin:vue/essential",
+    "eslint:recommended",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "indent": ["error", 2],
+    "quotes": ["error", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
+    "no-unused-vars": ["error", {"args": "none"}],
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
       ],
       env: {
-        mocha: true
-      }
-    }
-  ]
-}
+        mocha: true,
+      },
+    },
+  ],
+};
