ac6d9e7974bc9ddbfeb843ce76fa7a39fc619c7a
srad <saman@posteo.de>
2019-11-13 15:23:42 +0100

    Setting theme color as primary color, all routes rendered to navbar. * Now visible on landing page and navbar and all primary class usage with bootstrap. * All router elements are now automatically rendered in the navbar, there is no need to write them individually.

--- src/router.js
+++ src/router.js
@@ -5,23 +5,17 @@ import Game2 from "./views/Game2.vue";
 import Home from "./views/Home.vue";
 import Help from "./views/Help";
 import Leaderboard from "./views/Leaderboard.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {path: "/", name: "home", component: Home},
-    {path: "/game", name: "game", component: Game},
-    {path: "/game2", name: "game2", component: Game2},
-    {path: "/leaderboard", name: "leaderboard", component: Leaderboard},
-    {path: "/help", name: "help", component: Help},
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue"),
-    },
+    {path: "/", name: "home", component: Home, title: "Home"},
+    {path: "/game", name: "game", component: Game, title: "Game 1"},
+    {path: "/game2", name: "game2", component: Game2, title: "Game 2"},
+    {path: "/leaderboard", name: "leaderboard", component: Leaderboard, title: "Leaderboard"},
+    {path: "/help", name: "help", component: Help, title: "Help"},
+    {path: "/about", name: "about", component: About, title: "About"},
   ],
 });
