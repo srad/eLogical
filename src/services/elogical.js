59adaeed3c26de3606899e8f8e6e8b783f787e1e
srad <saman@posteo.de>
2019-11-15 03:07:49 +0100

    Adding production server URL to Vue env build file.

--- src/services/elogical.js
+++ src/services/elogical.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.VUE_APP_API_URL || "http://localhost:3000";
+const API_URL = process.env.VUE_APP_API_URL;
 const JWTName = "eLogicalAuthJWT";
 
 const store = window.localStorage;
