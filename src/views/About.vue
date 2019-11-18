14e2b4e9ef5d5fa45ce0117bbe4dc3de2311e9fe
Alexander Cornel <alexander.cornel@hotmail.de>
2019-11-18 20:26:55 +0100

    added About-page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1> -->
+    <h2>What is eLogical?</h2>
+    <p>eLogical is a web application for practicing the evaluation of boolean algebra expressions.</p>
+    <p>The app was developed by students of the Goethe-University Frankfurt am Main for the course "Platforms and Systems for eLearning" in the winter-semester 2019/20.</p>
+    <p>The layout is optimized for mobile devices.</p>
+    <!-- <h2>Developers:</h2>
+    <p></p> -->
+    <h2>List of frameworks and libraries used in eLogical:</h2>
+    <ul>
+      <li><a href="https://vuejs.org/">Vue.js</a></li>
+      <li><a href="https://bootstrap-vue.js.org/">BootstrapVue</a></li>
+      <li><a href="https://vuex.vuejs.org/">Vuex</a></li>
+      <li><a href="https://expressjs.com/">Express</a></li>
+      <li><a href="https://fontawesome.com/">Font Awesome</a></li>
+      <li><a href="https://katex.org/">Katex</a></li>
+      <li><a href="https://github.com/visjs/vis-network">vis-network</a></li>
+      <li><a href="https://github.com/axios/axios">axios</a></li>
+      <li><a href="https://www.chaijs.com/">Chai</a></li>
+    </ul>
   </div>
 </template>
