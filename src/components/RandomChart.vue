50f0030b0372a67ae1efc73cd8d72979ab870f20
Elucidateqt <bajrami@studiumdigitale.uni-frankfurt.de>
2020-01-26 19:59:20 +0100

    chartjs with example

---
+++ src/components/RandomChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="small" v-if="loaded">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+  import LineChart from '../lib/charts/LineChart.js'
+
+  export default {
+    name: 'RandomChart',
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null,
+        loaded: false
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#0066ff',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+        //only render the graph once we have data (especially important for async calls to api)
+        this.loaded = true
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -21,18 +21,37 @@
         </div>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col>
+        <random-chart></random-chart>
+
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+import RandomChart from '../components/RandomChart'
 export default {
   name: "Profile",
+  components: {RandomChart},
   data() {
     return {
       loading: true,
       entry: undefined,
       hasScore: false,
       localusername: localStorage.username,
+      charts: {
+        pointsHistory: {
+          chartData: {
+            labels: ['20.01.2020','21.01.2020','22.01.2020','23.01.2020','24.01.2020','25.01.2020'],
+            data: [15, 12, 17, 25, 26, 26]
+          },
+          options: {
+
+          }
+        }
+      }
     };
   },
   mounted() {
