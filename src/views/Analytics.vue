fc1c5e55955fc3a2a8d0a2e583d5c62d283ceaa6
Alexander Cornel <alexander.cornel@hotmail.de>
2020-02-14 11:32:47 +0100

    added empty dates to GamesByDate graph

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -137,23 +137,16 @@ export default {
         },
         mistakesByOperator: {
           data: {
-            labels: [
-              "AND",
-              "OR",
-              "IMPLICATION",
-              "NOT",
-              "XOR",
-              "EQUAL"
-            ],
+            labels: ["AND", "OR", "IMPLICATION", "NOT", "XOR", "EQUAL"],
             datasets: [
               {
                 label: "answered correctly",
-                backgroundColor: 'rgb(77, 186, 135)',
+                backgroundColor: "rgb(77, 186, 135)",
                 data: [0, 0, 0, 0, 0, 0]
               },
               {
                 label: "answered wrong",
-                backgroundColor: 'rgb(255, 99, 132)',
+                backgroundColor: "rgb(255, 99, 132)",
                 data: [0, 0, 0, 0, 0, 0]
               }
             ]
@@ -175,8 +168,12 @@ export default {
             labels: ["life", "reroll", "none"],
             datasets: [
               {
-                backgroundColor: ["rgb(77, 186, 135)", "rgb(255, 99, 132)", "rgb(60, 60, 60)"],
-                data: [0,0,0]
+                backgroundColor: [
+                  "rgb(77, 186, 135)",
+                  "rgb(255, 99, 132)",
+                  "rgb(60, 60, 60)"
+                ],
+                data: [0, 0, 0]
               }
             ]
           },
@@ -184,6 +181,58 @@ export default {
         }
       }
     };
+  },
+  methods: {
+    sortRunsByDate(finishedRunsByDate) {
+      finishedRunsByDate.sort(function(a, b) {
+        var dateA = a._id.day.toLowerCase(),
+          dateB = b._id.day.toLowerCase();
+        if (dateA < dateB) return -1;
+        if (dateA > dateB) return 1;
+        return 0;
+      });
+      return finishedRunsByDate;
+    },
+
+    genereateEmptyDayData(unsorted) {
+      var sorted = this.sortRunsByDate(unsorted);
+      const min = sorted[0]._id.day;
+      const max = sorted[sorted.length - 1]._id.day;
+      const minDate = new Date(min);
+      const maxDate = new Date(max);
+
+      const dateRange = this.getDates(minDate, maxDate);
+      const newDates = dateRange.filter(
+        e => !sorted.some(x => x._id.day === e)
+      );
+      newDates.forEach(date => {
+        const elem = { _id: { day: date, event: "game-end" }, frequency: 0 };
+        sorted.push(elem);
+      });
+      sorted = this.sortRunsByDate(sorted);
+      return sorted;
+    },
+
+    getDates(startDate, stopDate) {
+      var dateArray = new Array();
+      var currentDate = startDate;
+      while (currentDate <= stopDate) {
+        var newDate = new Date(currentDate);
+        dateArray.push(this.formateDate(newDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      return dateArray;
+    },
+
+    formateDate(date) {
+      var mm = date.getMonth() + 1;
+      var dd = date.getDate();
+      return [
+        date.getFullYear(),
+        (mm > 9 ? "" : "0") + mm,
+        (dd > 9 ? "" : "0") + dd
+      ].join("-");
+    }
   },
   mounted() {
     Promise.all([this.$api.getAnalytics()])
@@ -195,39 +244,50 @@ export default {
         const avgRuns = runsStarted / analytics.countUsers;
         const levelsCompleted = analytics.groupBySuccess[1].frequency;
 
-        const rewardsSelected = {}
+        const rewardsSelected = {};
         analytics.groupByLootSelected.forEach(el => {
-          if(el._id.loot === null){
-            rewardsSelected.none = el.frequency
-          }else{
-            rewardsSelected[el._id.loot] = el.frequency
+          if (el._id.loot === null) {
+            rewardsSelected.none = el.frequency;
+          } else {
+            rewardsSelected[el._id.loot] = el.frequency;
           }
-        })
-        if(!rewardsSelected.hasOwnProperty("heart")){
+        });
+        if (!rewardsSelected.hasOwnProperty("heart")) {
           rewardsSelected.heart = 0;
         }
-        if(!rewardsSelected.hasOwnProperty("none")){
+        if (!rewardsSelected.hasOwnProperty("none")) {
           rewardsSelected.none = 0;
         }
-        if(!rewardsSelected.hasOwnProperty("dice")){
+        if (!rewardsSelected.hasOwnProperty("dice")) {
           rewardsSelected.dice = 0;
         }
 
         // console.log("rewards formatted", rewardsSelected)
-        this.charts.rewardsSelected.data.datasets[0].data = [rewardsSelected.heart, rewardsSelected.dice, rewardsSelected.none]
+        this.charts.rewardsSelected.data.datasets[0].data = [
+          rewardsSelected.heart,
+          rewardsSelected.dice,
+          rewardsSelected.none
+        ];
 
-        const deathsByDifficulty = []
-        analytics.groupByGameEndAndDifficulty.forEach(el => deathsByDifficulty.push({"difficulty": el._id.difficulty, "frequency": el.frequency}))
+        const deathsByDifficulty = [];
+        analytics.groupByGameEndAndDifficulty.forEach(el =>
+          deathsByDifficulty.push({
+            difficulty: el._id.difficulty,
+            frequency: el.frequency
+          })
+        );
         //sort ascending by difficulty for viz
-        deathsByDifficulty.sort(function(a, b){return a.difficulty-b.difficulty});
+        deathsByDifficulty.sort(function(a, b) {
+          return a.difficulty - b.difficulty;
+        });
         // console.log("deaths sorted", deathsByDifficulty)
-        let difficultyComparisonData = []
+        let difficultyComparisonData = [];
         deathsByDifficulty.forEach(el => {
           this.charts.difficultyComparison.data.labels.push(el.difficulty);
           difficultyComparisonData.push(el.frequency);
-          });
+        });
         // console.log("deathsByDifficulty formatted", difficultyComparisonData)
-        this.charts.difficultyComparison.data.datasets[0].data = difficultyComparisonData
+        this.charts.difficultyComparison.data.datasets[0].data = difficultyComparisonData;
 
         let mistakeCount = {
             and: 0,
@@ -236,8 +296,7 @@ export default {
             xor: 0,
             implication: 0,
             eq: 0
-        },
-
+          },
           correctCount = {
             and: 0,
             or: 0,
@@ -245,25 +304,45 @@ export default {
             xor: 0,
             implication: 0,
             eq: 0
-        }
+          };
 
-        let correctByOperator = analytics.groupBySuccessAndOperator.filter(el => el._id.success === true)
+        let correctByOperator = analytics.groupBySuccessAndOperator.filter(
+          el => el._id.success === true
+        );
         // console.log("correctByOperator", correctByOperator)
-        correctByOperator.forEach(
-          el => el._id.op.forEach(op => correctCount[op] = correctCount[op] +  el.frequency)
-        )
+        correctByOperator.forEach(el =>
+          el._id.op.forEach(
+            op => (correctCount[op] = correctCount[op] + el.frequency)
+          )
+        );
         // console.log("correctCount", correctCount)
-        this.charts.mistakesByOperator.data.datasets[0].data = [correctCount.and, correctCount.or, correctCount.implication,
-        correctCount.not, correctCount.xor, correctCount.eq]
+        this.charts.mistakesByOperator.data.datasets[0].data = [
+          correctCount.and,
+          correctCount.or,
+          correctCount.implication,
+          correctCount.not,
+          correctCount.xor,
+          correctCount.eq
+        ];
 
-        let mistakesByOperator = analytics.groupBySuccessAndOperator.filter(el => el._id.success === false)
+        let mistakesByOperator = analytics.groupBySuccessAndOperator.filter(
+          el => el._id.success === false
+        );
         // console.log("mistakesByOperator", mistakesByOperator)
-        mistakesByOperator.forEach(
-          el => el._id.op.forEach(op => mistakeCount[op] = mistakeCount[op] +  el.frequency)
-        )
+        mistakesByOperator.forEach(el =>
+          el._id.op.forEach(
+            op => (mistakeCount[op] = mistakeCount[op] + el.frequency)
+          )
+        );
         // console.log("mistakeCount", mistakeCount)
-        this.charts.mistakesByOperator.data.datasets[1].data = [mistakeCount.and, mistakeCount.or, mistakeCount.implication,
-        mistakeCount.not, mistakeCount.xor, mistakeCount.eq]
+        this.charts.mistakesByOperator.data.datasets[1].data = [
+          mistakeCount.and,
+          mistakeCount.or,
+          mistakeCount.implication,
+          mistakeCount.not,
+          mistakeCount.xor,
+          mistakeCount.eq
+        ];
 
         document.getElementById("spanRunsStarted").innerText = runsStarted;
         document.getElementById("spanAvgRuns").innerText = avgRuns.toFixed(2);
@@ -271,17 +350,11 @@ export default {
           "spanLevelsCompleted"
         ).innerText = levelsCompleted;
 
-        const finishedRunsByDate = analytics.groupEventsByDay.filter(
+        var finishedRunsByDate = analytics.groupEventsByDay.filter(
           x => x._id.event == "game-end"
         );
 
-        finishedRunsByDate.sort(function(a, b) {
-          var dateA = a._id.day.toLowerCase(),
-            dateB = b._id.day.toLowerCase();
-          if (dateA < dateB) return -1;
-          if (dateA > dateB) return 1;
-          return 0;
-        });
+        finishedRunsByDate = this.genereateEmptyDayData(finishedRunsByDate);
 
         this.charts.gamesByDate.data.labels = finishedRunsByDate.map(
           x => x._id.day
